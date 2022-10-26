0c14fab0915351ca4fe7a9813b5c7fa9a165ae84
dzq <x_mitsui@163.com>
2022-10-26 21:05:17 +0800

    引入vantUI

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { App } from './App'
 import { router } from './router'
+import 'vant/lib/index.css'
 import '@svgstore'
 
 createApp(App).use(router).mount('#app')
