ce04e5f5116dcd7fea3398f11805b1fac4153eff
dzq <x_mitsui@163.com>
2022-10-27 17:36:49 +0800

    点击""出现栈溢出，页面卡死

--- src/components/tag/EmojiSelected.tsx
+++ src/components/tag/EmojiSelected.tsx
@@ -1,19 +1,26 @@
-import { defineComponent } from 'vue'
+import { defineComponent, PropType, ref } from 'vue'
 import s from './EmojiSelected.module.scss'
 export const EmojiSelected = defineComponent({
   name: 'EmojiSelected',
   props: {
-    contents: {
-      type: String,
+    emojiTable: {
+      type: Array as PropType<[string, string[]][]>,
       required: true
     }
   },
   setup(props, context) {
+    const refIndex = ref(0)
+    const selectEmoji = (index: string) => {
+      refIndex.value = parseInt(index)
+    }
     return () => (
       <>
         <ul>
-          <li></li>
+          {props.emojiTable.map((emojiItem) => {
+            return <li onClick={() => selectEmoji(emojiItem[0])}>{emojiItem[0]}</li>
+          })}
         </ul>
+        <div>{props.emojiTable[refIndex.value][1]}</div>
       </>
     )
   }
--- src/components/tag/TagCreate.tsx
+++ src/components/tag/TagCreate.tsx
@@ -3,90 +3,14 @@ import { useRouter } from 'vue-router'
 import { MainLayout } from '../../layouts/MainLayout'
 import { Icon } from '../../shared/Icon'
 import { EmojiSelected } from './EmojiSelected'
-import { emojiList } from '../../shared/emojiList'
+import { emojiTable } from './emoji/EmojiTable'
 import s from './TagCreate.module.scss'
 export const TagCreate = defineComponent({
   name: 'TagCreate',
   setup(props, context) {
     const refSelectedValue = ref('')
     const router = useRouter()
-    const table: [string, string[]][] = [
-      [
-        '表情',
-        [
-          'face-smiling',
-          'face-affection',
-          'face-tongue',
-          'face-hand',
-          'face-neutral-skeptical',
-          'face-sleepy',
-          'face-unwell',
-          'face-hat',
-          'face-glasses',
-          'face-concerned',
-          'face-negative',
-          'face-costume'
-        ]
-      ],
-      [
-        '手势',
-        [
-          'hand-fingers-open',
-          'hand-fingers-partial',
-          'hand-single-finger',
-          'hand-fingers-closed',
-          'hands',
-          'hand-prop',
-          'body-parts'
-        ]
-      ],
-      [
-        '人物',
-        [
-          'person',
-          'person-gesture',
-          'person-role',
-          'person-fantasy',
-          'person-activity',
-          'person-sport',
-          'person-resting'
-        ]
-      ],
-      ['衣服', ['clothing']],
-      [
-        '动物',
-        [
-          'cat-face',
-          'monkey-face',
-          'animal-mammal',
-          'animal-bird',
-          'animal-amphibian',
-          'animal-reptile',
-          'animal-marine',
-          'animal-bug'
-        ]
-      ],
-      ['植物', ['plant-flower', 'plant-other']],
-      ['自然', ['sky & weather', 'science']],
-      [
-        '食物',
-        ['food-fruit', 'food-vegetable', 'food-prepared', 'food-asian', 'food-marine', 'food-sweet']
-      ],
-      ['运动', ['sport', 'game']]
-    ]
-    const newTable: Record<string, string[]> = {}
-    table.map((item) => {
-      const alias = item[1]
-      const box: string[] = []
-      alias.forEach((aItem) => {
-        const findz = emojiList.find((emojiItem) => emojiItem[0] === aItem)
-        if (findz) {
-          box.push(...findz[1])
-        }
-      })
-      newTable[item[0]] = box
-    })
-    console.log(newTable[0])
+
     const onUpdateValue = (val: string) => {
       refSelectedValue.value = val
     }
@@ -106,9 +30,8 @@ export const TagCreate = defineComponent({
                   <input type="text" />
                 </label>
                 <label>
-                  <span>符号</span>
-                  <span>符合</span>
-                  <EmojiSelected></EmojiSelected>
+                  <span>符号 {}</span>
+                  <EmojiSelected emojiTable={emojiTable}></EmojiSelected>
                 </label>
               </form>
             </>
