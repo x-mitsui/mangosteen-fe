77d635c202d747698c08a54b2d5424c799dd8203
dzq <x_mitsui@163.com>
2022-11-08 19:34:20 +0800

    表单验证器

---
+++ src/shared/validate.ts
@@ -0,0 +1,40 @@
+interface FormData {
+  [k: string]: string | number | null | undefined | FormData
+}
+type Rule<T extends FormData> = {
+  key: keyof T
+  message: string
+} & ({ type: 'required' } | { type: 'pattern'; reg: RegExp })
+type Rules<T extends FormData> = Rule<T>[]
+
+const validate = <T extends FormData>(formData: T, rules: Rules<T>) => {
+  type ErrorType = {
+    [k in keyof T]?: string[]
+  }
+  const errors: ErrorType = {}
+  rules.forEach((rule) => {
+    const { key, message, type } = rule
+    const value = formData[key]
+    switch (type) {
+      case 'required':
+        if (!value) {
+          errors[key] = errors[key] ?? []
+          errors[key]?.push(message)
+        }
+        break
+      case 'pattern':
+        // 这里ts推测错误，这里断言value非空
+        if (!isEmpty(value) && rule.reg.test(value!.toString())) {
+          errors[key]?.push(rule.message)
+        }
+        break
+    }
+  })
+  return errors
+}
+const isEmpty = (value: string | number | null | undefined | FormData) => {
+  return value === '' || value === null || value === undefined
+}
+
+export type { Rules, Rule }
+export { validate }
