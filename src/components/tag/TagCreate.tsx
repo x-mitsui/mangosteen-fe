96d45242cdf2ce01716b5a34ce0a9a635fc4ca62
dzq <x_mitsui@163.com>
2022-11-08 19:35:05 +0800

    TagCreate页面创建

--- src/components/tag/TagCreate.tsx
+++ src/components/tag/TagCreate.tsx
@@ -1,4 +1,4 @@
-import { defineComponent, ref } from 'vue'
+import { defineComponent, reactive, ref } from 'vue'
 import { useRouter } from 'vue-router'
 import { MainLayout } from '../../layouts/MainLayout'
 import { Icon } from '../../shared/Icon'
@@ -6,17 +6,47 @@ import { EmojiSelected } from './EmojiSelected'
 import { emojiTable } from './emoji/EmojiTable'
 import s from './TagCreate.module.scss'
 import { Button } from '../../shared/Button'
+import { Rule, Rules, validate } from '../../shared/validate'
 export const TagCreate = defineComponent({
   name: 'TagCreate',
   setup(props, context) {
-    const refSelectedValue = ref('')
     const router = useRouter()
-
-    const onUpdateValue = (val: string) => {
-      refSelectedValue.value = val
-    }
+    const formData = reactive({
+      labelName: '',
+      emoji: ''
+    })
+    const errors = ref<{
+      [k in keyof typeof formData]?: string[]
+    }>()
+    // const onUpdateValue = (val: string) => {
+    //   console.log('呼噜噜')
+    //   formData.emoji = val
+    // }
     const goBack = () => {
       router.push('/start')
+    }
+    const onSubmit = (e: Event) => {
+      e.preventDefault()
+      const rule1: Rule<typeof formData> = {
+        message: '须填2到4个字符',
+        key: 'labelName',
+        type: 'pattern',
+        reg: /^.{1,4}$/g
+      }
+      const rule2: Rule<typeof formData> = {
+        message: '必填',
+        key: 'labelName',
+        type: 'required'
+      }
+      const rule3: Rule<typeof formData> = {
+        message: '必填',
+        key: 'emoji',
+        type: 'required'
+      }
+      // const rules: Rules<typeof formData> = [rule1, rule2, rule3]
+
+      errors.value = validate<typeof formData>(formData, [rule2, rule1, rule3])
+      console.log('1111:', errors)
     }
     return () => (
       <MainLayout>
@@ -24,27 +54,39 @@ export const TagCreate = defineComponent({
           title: () => '新建标签',
           icon: (kclass: string) => <Icon name="back" class={kclass} onClick={goBack} />,
           main: () => (
-            <>
-              <form class={s.wrapper}>
-                <label class={s.label}>
-                  <span class={s.title}>标签名</span>
-                  <input type="text" class={s.input} placeholder="2到4个汉字" />
-                </label>
-                <label class={s.label}>
-                  <span class={s.title}>符号：{refSelectedValue.value}</span>
-                  <EmojiSelected
-                    emojiTable={emojiTable}
-                    v-model={refSelectedValue.value}
-                  ></EmojiSelected>
-                </label>
-                <p class={s.tips}>记账时长按标签即可进行编辑</p>
-                <div class={s.formRow}>
-                  <div class={s.formItem_value}>
-                    <Button class={[s.formItem, s.button]}>确定</Button>
-                  </div>
+            <form class={s.wrapper}>
+              <label class={s.label}>
+                <span class={s.formItem_name}>标签名</span>
+                <div class={s.formItem_value}>
+                  <input
+                    type="text"
+                    class={s.formInput}
+                    placeholder="2到4个汉字"
+                    v-model={formData.labelName}
+                  />
                 </div>
-              </form>
-            </>
+                <div class={s.formItem_error}>
+                  {errors.value?.labelName?.[0] ? errors.value?.labelName?.[0] : '\u3000'}
+                </div>
+              </label>
+              <label class={s.label}>
+                <span class={[s.formItem_name, 'emoji-font-conf']}>符号：{formData.emoji}</span>
+                <div class={s.formItem_value}>
+                  <EmojiSelected emojiTable={emojiTable} v-model={formData.emoji}></EmojiSelected>
+                </div>
+                <div class={s.formItem_error}>
+                  {errors.value?.emoji?.[0] ? errors.value?.emoji?.[0] : '\u3000'}
+                </div>
+              </label>
+              <p class={s.tips}>记账时长按标签即可进行编辑</p>
+              <div class={s.formRow}>
+                <div class={s.formItem_value}>
+                  <Button class={[s.formItem, s.button]} onClick={onSubmit}>
+                    确定
+                  </Button>
+                </div>
+              </div>
+            </form>
           )
         }}
       </MainLayout>
