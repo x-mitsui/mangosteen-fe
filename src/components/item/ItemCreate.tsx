80b3ce2961e0c0ea198fa2bf4b4b50520fc058a4
dzq <x_mitsui@163.com>
2022-11-08 21:12:33 +0800

    wip: ItemCreate page 制作中

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -4,10 +4,57 @@ import { MainLayout } from '../../layouts/MainLayout'
 import { Icon } from '../../shared/Icon'
 import { Tab, Tabs } from '../../shared/Tabs'
 import { InfoInput } from './InfoInput'
+import s from './ItemCreate.module.scss'
 export const ItemCreate = defineComponent({
   setup(props, context) {
     const refSelectedValue = ref('')
     const router = useRouter()
+    const refExpensesTags = ref([
+      { id: 1, name: '餐费', sign: '￥', category: 'expenses' },
+      { id: 2, name: '打车', sign: '￥', category: 'expenses' },
+      { id: 3, name: '聚餐', sign: '￥', category: 'expenses' },
+      { id: 4, name: '打车', sign: '￥', category: 'expenses' },
+      { id: 5, name: '聚餐', sign: '￥', category: 'expenses' },
+      { id: 6, name: '打车', sign: '￥', category: 'expenses' },
+      { id: 7, name: '聚餐', sign: '￥', category: 'expenses' }
+    ])
+    const refIncomeTags = ref([
+      { id: 4, name: '工资', sign: '￥', category: 'income' },
+      { id: 5, name: '彩票', sign: '￥', category: 'income' },
+      { id: 6, name: '滴滴', sign: '￥', category: 'income' },
+      { id: 11, name: '彩票', sign: '￥', category: 'income' },
+      { id: 18, name: '滴滴', sign: '￥', category: 'income' },
+      { id: 17, name: '彩票', sign: '￥', category: 'income' },
+      { id: 19, name: '滴滴', sign: '￥', category: 'income' },
+      { id: 4, name: '工资', sign: '￥', category: 'income' },
+      { id: 5, name: '彩票', sign: '￥', category: 'income' },
+      { id: 6, name: '滴滴', sign: '￥', category: 'income' },
+      { id: 11, name: '彩票', sign: '￥', category: 'income' },
+      { id: 18, name: '滴滴', sign: '￥', category: 'income' },
+      { id: 17, name: '彩票', sign: '￥', category: 'income' },
+      { id: 19, name: '滴滴', sign: '￥', category: 'income' },
+      { id: 4, name: '工资', sign: '￥', category: 'income' },
+      { id: 5, name: '彩票', sign: '￥', category: 'income' },
+      { id: 6, name: '滴滴', sign: '￥', category: 'income' },
+      { id: 11, name: '彩票', sign: '￥', category: 'income' },
+      { id: 18, name: '滴滴', sign: '￥', category: 'income' },
+      { id: 17, name: '彩票', sign: '￥', category: 'income' },
+      { id: 19, name: '滴滴', sign: '￥', category: 'income' },
+      { id: 4, name: '工资', sign: '￥', category: 'income' },
+      { id: 5, name: '彩票', sign: '￥', category: 'income' },
+      { id: 6, name: '滴滴', sign: '￥', category: 'income' },
+      { id: 11, name: '彩票', sign: '￥', category: 'income' },
+      { id: 18, name: '滴滴', sign: '￥', category: 'income' },
+      { id: 17, name: '彩票', sign: '￥', category: 'income' },
+      { id: 19, name: '滴滴', sign: '￥', category: 'income' },
+      { id: 4, name: '工资', sign: '￥', category: 'income' },
+      { id: 5, name: '彩票', sign: '￥', category: 'income' },
+      { id: 6, name: '滴滴', sign: '￥', category: 'income' },
+      { id: 11, name: '彩票', sign: '￥', category: 'income' },
+      { id: 18, name: '滴滴', sign: '￥', category: 'income' },
+      { id: 17, name: '彩票', sign: '￥', category: 'income' },
+      { id: 19, name: '滴滴', sign: '￥', category: 'income' }
+    ])
     const onUpdateValue = (val: string) => {
       refSelectedValue.value = val
     }
@@ -22,8 +69,15 @@ export const ItemCreate = defineComponent({
           main: () => (
             <>
               <Tabs selected={refSelectedValue.value} onUpdateValue={onUpdateValue}>
-                <Tab name="income">收入</Tab>
-                <Tab name="pay">支出</Tab>
+                <Tab name="收入">
+                  <div class={s.tag}>
+                    <div class={s.sign}>
+                      <Icon name="add"></Icon>
+                    </div>
+                    <span class={s.name}>新增</span>
+                  </div>
+                </Tab>
+                <Tab name="支出">支出</Tab>
               </Tabs>
               <InfoInput></InfoInput>
             </>
