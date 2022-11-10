a82d611fc196ef7b8e206c0c8493d3cbd7a84582
dzq <x_mitsui@163.com>
2022-11-10 15:44:35 +0800

    标签头改为磁吸

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -6,8 +6,8 @@ export const MainLayout = defineComponent({
   setup(props, context) {
     const { title, icon, main } = context.slots
     return () => (
-      <div>
-        <NavBar>
+      <div class={s.main_layout}>
+        <NavBar class={s.nav_bar}>
           {{
             default: title,
             icon
