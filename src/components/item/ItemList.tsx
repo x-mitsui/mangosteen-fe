278bad98aa07ab9787b1294f9daa86c88ac6b7b0
dzq <x_mitsui@163.com>
2022-11-11 20:29:04 +0800

    wip: itemList

--- src/components/item/ItemList.tsx
+++ src/components/item/ItemList.tsx
@@ -1,11 +1,25 @@
+import { Overlay } from 'vant'
 import { defineComponent, ref } from 'vue'
 import { MainLayout } from '../../layouts/MainLayout'
 import { Icon } from '../../shared/Icon'
 import { Tab, Tabs } from '../../shared/Tabs'
+import { Time } from '../../shared/time'
 import s from './ItemList.module.scss'
+import { ItemSummary } from './ItemSummary'
 export const ItemList = defineComponent({
   setup(props, context) {
     const refTabSelected = ref('本月')
+    const customOverlayShow = ref(false)
+    const time = new Time()
+    const customDate = new Time()
+    const dateList = [
+      { start: time.firstDayOfMonth(), end: time.lastDayOfMonth() },
+      {
+        start: time.add(-1, 'month').firstDayOfMonth(),
+        end: time.add(-1, 'month').lastDayOfMonth()
+      },
+      { start: time.firstDayOfYear(), end: time.lastDayOfYear() }
+    ]
     return () => (
       <MainLayout>
         {{
@@ -13,11 +27,43 @@ export const ItemList = defineComponent({
           icon: (kclass: string) => <Icon name="menu" class={kclass}></Icon>,
           main: () => (
             <div>
-              <Tabs v-model:selected={refTabSelected.value} classPrefix="customTabs">
-                <Tab name="本月">本月</Tab>
-                <Tab name="上个月">上个月</Tab>
-                <Tab name="今年">今年</Tab>
-                <Tab name="自定义起始时间">自定义起始时间</Tab>
+              <Tabs
+                selected={refTabSelected.value}
+                classPrefix="customTabs"
+                onUpdate:selected={(name: string) => {
+                  refTabSelected.value = name
+                  if (name === '自定义时间') {
+                    customOverlayShow.value = true
+                  }
+                }}
+              >
+                <Tab name="本月">
+                  <ItemSummary
+                    startDate={dateList[0].start.format()}
+                    endDate={dateList[0].end.format()}
+                  />
+                </Tab>
+                <Tab name="上个月">
+                  <ItemSummary
+                    startDate={dateList[0].start.format()}
+                    endDate={dateList[0].end.format()}
+                  />
+                </Tab>
+                <Tab name="今年">
+                  <ItemSummary
+                    startDate={dateList[0].start.format()}
+                    endDate={dateList[0].end.format()}
+                  />
+                </Tab>
+                <Tab name="自定义时间">
+                  <Overlay
+                    show={customOverlayShow.value}
+                    onClick={() => {
+                      customOverlayShow.value = false
+                    }}
+                  ></Overlay>
+                  {/* <ItemSummary startDate={customDate.format()} endDate={customDate.format()} /> */}
+                </Tab>
               </Tabs>
             </div>
           )
