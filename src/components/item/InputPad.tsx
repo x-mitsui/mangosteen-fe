0f2be58ec2ef384c454a2b3d6cf13b054b71c085
dzq <x_mitsui@163.com>
2022-12-05 19:02:03 +0800

    改个名，给组件添加了数据暴露

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -1,15 +1,19 @@
 import { defineComponent, ref } from 'vue'
-import s from './InfoInput.module.scss'
+import s from './InputPad.module.scss'
 import { DatetimePicker, Popup, Switch } from 'vant'
 import { Icon } from '../../shared/Icon'
 
-export const InfoInput = defineComponent({
+export const InputPad = defineComponent({
+  props: {
+    happenAt: String,
+    amount: Number
+  },
   setup(props, context) {
     const minDate = new Date(2020, 0, 1)
     const maxDate = new Date(2025, 0, 1)
     const refInfoValue = ref({
       time: new Date(),
-      money: '0'
+      money: props.amount === undefined ? '0' : (props.amount / 100).toString()
     })
 
     const refShowPop = ref(false)
@@ -32,9 +36,10 @@ export const InfoInput = defineComponent({
       } else if (key === '删除') {
         refInfoValue.value.money = '0'
       } else {
+        context.emit('update:amount', parseFloat(refInfoValue.value.money) * 100)
       }
     }
-    const { emit } = context
+
     return () => (
       <div class={s.wrapper}>
         <div class={s.showWrapper}>
@@ -62,6 +67,7 @@ export const InfoInput = defineComponent({
             onConfirm={(date: Date) => {
               refInfoValue.value.time = date
               refShowPop.value = false
+              context.emit('update:happen-at', refInfoValue.value.time.toISOString())
             }}
           ></DatetimePicker>
         </Popup>
